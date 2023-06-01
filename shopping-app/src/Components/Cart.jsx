090d73dc0d6e51758e38f6db151406e4b1d9e9d6
aaliboyaci <aaliboyaci@gmail.com>
2023-06-02 02:55:57 +0300

    add remove item feature

--- shopping-app/src/Components/Cart.jsx
+++ shopping-app/src/Components/Cart.jsx
@@ -7,24 +7,34 @@ export default function Cart() {
     const { setPage } = useContext(ProductContext);
     const totalPrice = cart.reduce((total, item) => total + item.price, 0);
 
+    const removeFromCart = (itemId) => {
+        const updatedCart = cart.filter((item) => item.id !== itemId);
+        setCart(updatedCart);
+    };
+
     return (
         <div>
-            <h2>Cart</h2>
-            {cart.length > 0 ? <button onClick={() => setCart([])}>Clear Cart</button> : <button onClick={() => setPage(0)}>Back to shopping</button>}
+            <h1>Cart</h1>
+            {cart.length > 0 ? (<>
+                <button className="clearCart" onClick={() => setCart([])}>Clear Cart</button>
+                <br /> <br /></>
+            ) : (
+                <button className='goToCart' onClick={() => setPage(0)}>Back to shopping</button>
+            )}
             <div>
                 {cart.map((item) => (
                     <div className="cartItem" key={item.id}>
                         <img className="cartImage" src={item.image} alt={item.title} />
-
                         <p>{item.title}</p>
-                        <h2>Price: ${item.price}</h2>
-
+                        <div className="pricetag">
+                            <h2>Price: ${item.price}</h2>
+                            <p className="clearCart" onClick={() => removeFromCart(item.id)}><i>Remove item</i></p>
+                        </div>
                     </div>
                 ))}
             </div>
-<br></br>
-<h3>Total Price: ${totalPrice.toFixed(2)}</h3>
-
+            <br />
+            <h1>Total Price: ${totalPrice.toFixed(2)}</h1>
         </div>
     );
 }
